22ac64ed82de114e61245fa57eb823b4a94cb134
Han Myo Aung <hanmyoaung848495@gmail.com>
2026-06-20 10:52:28 +0630

    feat: integrate monetization ad scripts
    Add third-party ad tag to index and configure service worker for monetization support.

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11175466
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
